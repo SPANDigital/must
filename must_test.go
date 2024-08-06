5a0cc25b115bb40711cc2e606b519dee3b31ce84
Richard Wooding <richard.wooding@spandigital.com>
2024-08-06 16:32:25 +0200

    Fixed unit tests

--- must_test.go
+++ must_test.go
@@ -1,7 +1,7 @@
 package must
 
 import (
-	"net/url"
+	"errors"
 	"reflect"
 	"testing"
 )
@@ -17,13 +17,42 @@ func TestMustURL(t *testing.T) {
 		want      T
 		wantPanic bool
 	}
-	tests := []testCase[*url.URL]{
-		// TODO: Add test cases.
+	tests := []testCase[int]{
+		{
+			name: "failure",
+			args: args[int]{
+				v:   1,
+				err: nil,
+			},
+			want:      1,
+			wantPanic: false,
+		},
+		{
+			name: "success",
+			args: args[int]{
+				v:   0,
+				err: errors.New("some error"),
+			},
+			wantPanic: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Must(tt.args.v, tt.args.err); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Must() = %v, want %v", got, tt.want)
+			panicked := false
+			var got int
+			func() {
+				defer func() {
+					if r := recover(); r != nil {
+						panicked = true
+					}
+				}()
+				got = Must(tt.args.v, tt.args.err)
+			}()
+			if got != tt.want {
+				t.Errorf("got %v, want %v", got, tt.want)
+			}
+			if panicked != tt.wantPanic {
+				t.Errorf("Panicked = %v, wantPanic %v", panicked, tt.wantPanic)
 			}
 		})
 	}
@@ -36,55 +65,56 @@ func TestMust2(t *testing.T) {
 		err error
 	}
 	type testCase[T1 any, T2 any] struct {
-		name  string
-		args  args[T1, T2]
-		want  T1
-		want1 T2
+		name      string
+		args      args[T1, T2]
+		want1     T1
+		want2     T2
+		wantPanic bool
 	}
-	tests := []testCase[ /* TODO: Insert concrete types here */ ]{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, got1 := Must2(tt.args.v1, tt.args.v2, tt.args.err)
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Must2() got = %v, want %v", got, tt.want)
-			}
-			if !reflect.DeepEqual(got1, tt.want1) {
-				t.Errorf("Must2() got1 = %v, want %v", got1, tt.want1)
-			}
-		})
-	}
-}
-
-func TestMust3(t *testing.T) {
-	type args[T1 any, T2 any, T3 any] struct {
-		v1  T1
-		v2  T2
-		v3  T3
-		err error
-	}
-	type testCase[T1 any, T2 any, T3 any] struct {
-		name  string
-		args  args[T1, T2, T3]
-		want  T1
-		want1 T2
-		want2 T3
-	}
-	tests := []testCase[ /* TODO: Insert concrete types here */ ]{
-		// TODO: Add test cases.
+	tests := []testCase[int, int]{
+		{
+			name: "success",
+			args: args[int, int]{
+				v1:  1,
+				v2:  2,
+				err: nil,
+			},
+			want1:     1,
+			want2:     2,
+			wantPanic: false,
+		},
+		{
+			name: "failure",
+			args: args[int, int]{
+				v1:  1,
+				v2:  2,
+				err: errors.New("some error"),
+			},
+			want1:     0,
+			want2:     0,
+			wantPanic: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, got1, got2 := Must3(tt.args.v1, tt.args.v2, tt.args.v3, tt.args.err)
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Must3() got = %v, want %v", got, tt.want)
-			}
+			panicked := false
+			var got1, got2 int
+			func() {
+				defer func() {
+					if r := recover(); r != nil {
+						panicked = true
+					}
+				}()
+				got1, got2 = Must2(tt.args.v1, tt.args.v2, tt.args.err)
+			}()
 			if !reflect.DeepEqual(got1, tt.want1) {
-				t.Errorf("Must3() got1 = %v, want %v", got1, tt.want1)
+				t.Errorf("Must2() got1 = %v, want2 %v", got1, tt.want1)
 			}
 			if !reflect.DeepEqual(got2, tt.want2) {
-				t.Errorf("Must3() got2 = %v, want %v", got2, tt.want2)
+				t.Errorf("Must2() got2 = %v, want2 %v", got1, tt.want2)
+			}
+			if panicked != tt.wantPanic {
+				t.Errorf("Panicked = %v, wantPanic %v", panicked, tt.wantPanic)
 			}
 		})
 	}
